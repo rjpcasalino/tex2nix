d9b8f3b738c06eeafb5fd072e93f6a1b24856a37
Ryan Casalino <ryan@rjpc.net>
2023-12-19 14:14:11 -0800

    Update __init__.py

--- tex2nix/__init__.py
+++ tex2nix/__init__.py
@@ -14,6 +14,7 @@ def get_nix_packages() -> Set[str]:
         [
             "nix",
             "eval",
+            "--json",
             "nixpkgs#texlive",
             "--apply",
             "builtins.attrNames",
